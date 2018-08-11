0b7c2b187a64289cc42141fe5d13132c3104fb8b
DHGiang1 <dhgiang1@cmc.com.vn>
2018-08-11 10:49:19 +0700

    Đổi ico

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -7,7 +7,7 @@ class Header extends Component {
                 <div className="masthead-content">
                     <div className="container">
                         <h1 className="masthead-heading mb-0">Friend Forever</h1>
-                        <h2 className="masthead-subheading mb-0">Những hình ảnh đẹp</h2>
+                        <h2 className="masthead-subheading mb-0">Những hình ảnh đẹp về những người bạn của tôi</h2>
                         <a href="https://www.facebook.com/vuachom94" className="btn btn-primary btn-xl rounded-pill mt-5">Xem thêm</a>
                     </div>
                 </div>
