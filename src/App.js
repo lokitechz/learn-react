c4c82341a2fb180eb8a6fb65162685d8f6b74d25
DHGiang1 <dhgiang1@cmc.com.vn>
2018-08-13 20:47:29 +0700

    done

--- src/App.js
+++ src/App.js
@@ -11,9 +11,11 @@ class App extends Component {
       <div className="App">
         <TopMenu />
         <Header/>
-        <Content title='Mai Anh Thư' position1='order-lg-2' src='img/mat.jpg' noidung='Nickname bà chằn nặc nô' facebook='https://www.facebook.com/anhthu.cva.dav'/>
+        <Content title='Mai Anh Thư' position1='order-lg-2' src='img/mat.jpg' noidung='Nickname Nặc Nô' facebook='https://www.facebook.com/anhthu.cva.dav'/>
         <Content title='Nguyễn Thu Thủy' src='img/ntt.jpg' noidung='Nickname Thủy Tít' facebook='https://www.facebook.com/nguyenthuthuy2611'/>
         <Content title='Vũ Lệ Thủy' position1='order-lg-2' src='img/vlt.jpg' noidung='Nickname Tỉn' facebook='https://www.facebook.com/thuyvu285'/>
+        <Content title='Lê Đạt' src='img/dat.jpg' noidung='Nickname Đạt khoai' facebook='https://www.facebook.com/ledat.upem'/>
+        <Content title='Nam' position1='order-lg-2' src='img/nam.jpg' noidung='Nickname Dav' facebook='https://www.facebook.com/thanhnam.vu.946'/>
         <Footer/>
       </div>
     );
--- src/Component/Content/Content.js
+++ src/Component/Content/Content.js
@@ -1,54 +1,6 @@
 import React, { Component } from "react";
 
 class Content extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      status: 0
-    };
-  }
-
-  renderButton = () => (
-    <div className="col-12">
-      <div className="btn btn-group">
-        <button type="button" className="btn btn-info no-border-radius mgr-25" onClick={() => this.changeStatusTo1()}>
-          Xem thông tin
-        </button>
-        <button type="button" className="btn btn-danger no-border-radius">
-          Xóa thông tin
-        </button>
-      </div>
-    </div>
-  );
-
-  renderForm = () => (
-    <div className="col-12">
-      <form className="form-inline">
-        <div className="form-group mx-sm-3 mb-2">
-          <input type="text" className="form-control" />
-        </div>
-        <button type="button" className="btn btn-success mb-2 no-border-radius" onClick={() => this.changeStatusTo0()}>
-          Lưu thông tin
-        </button>
-      </form>
-    </div>
-  );
-
-  checkStatus = () => {
-    if (this.state.status === 0) {
-      return this.renderButton();
-    }else{
-      return this.renderForm();
-    }
-  };
-
-  changeStatusTo1 = () =>{
-    this.setState({status:1});
-  }
-  changeStatusTo0 = () =>{
-    this.setState({status:0});
-  }
-
   render() {
     return (
       <section>
@@ -63,8 +15,12 @@ class Content extends Component {
               <div className="p-5">
                 <h2 className="display-4">{this.props.title}</h2>
                 <p>{this.props.noidung}</p>
-                <a href={this.props.facebook}>Facebook</a>
-                {this.checkStatus()}
+                <a href={this.props.facebook} className="mb-25">Facebook</a>
+                <div className="col-12">
+                  <button type="button" className="btn btn-info no-border-radius">
+                    Xem chi tiết
+                  </button>
+                </div>
               </div>
             </div>
           </div>
